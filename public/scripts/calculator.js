112281c369afe613d5f55b0e75546d21c16fbd1a
Axeinator <33792363+Axeinator@users.noreply.github.com>
2021-01-12 12:57:57 -0600

    stylesheet and bootstrap

--- public/scripts/calculator.js
+++ public/scripts/calculator.js
@@ -4,10 +4,12 @@ function wAvg() {
   c3 = document.forms.grades.c3.valueAsNumber
   goal = document.forms.grades.goal.valueAsNumber
   required = ((goal - ((c1 * 0.3) + (c2 * 0.3) + (c3 * 0.3)))/0.1)
+  console.log(required)
   if (required < 0) {
-    document.getElementById('requiredScore').innerHTML = `Your required final exam score is 0%}`
+    console.log("inside")
+    document.getElementById('requiredScore').innerHTML = `Your required final exam score is 0%`
   }
-  if (required > 100) {
+  else if (required > 100) {
     document.getElementById('requiredScore').innerHTML = `Your required final exam score is ${required.toFixed(2)}% <br/>
     You probably can't get this.`
   }
