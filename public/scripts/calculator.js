c08961030334f9bfb06ed73a0a3fc29d6887bc63
Axeinator <33792363+Axeinator@users.noreply.github.com>
2021-01-12 12:33:03 -0600

    JS function

---
+++ public/scripts/calculator.js
@@ -0,0 +1,17 @@
+function wAvg() {
+  c1 = document.forms.grades.c1.valueAsNumber
+  c2 = document.forms.grades.c2.valueAsNumber
+  c3 = document.forms.grades.c3.valueAsNumber
+  goal = document.forms.grades.goal.valueAsNumber
+  required = ((goal - ((c1 * 0.3) + (c2 * 0.3) + (c3 * 0.3)))/0.1)
+  if (required < 0) {
+    document.getElementById('requiredScore').innerHTML = `Your required final exam score is 0%}`
+  }
+  if (required > 100) {
+    document.getElementById('requiredScore').innerHTML = `Your required final exam score is ${required.toFixed(2)}% <br/>
+    You probably can't get this.`
+  }
+  else {
+    document.getElementById('requiredScore').innerHTML = `Your required final exam score is ${required.toFixed(2)}%`
+  }
+}
