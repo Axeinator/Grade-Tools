c60fc6c47406fdfca246ef41b594ce55834de8b0
Axeinator <33792363+Axeinator@users.noreply.github.com>
2021-01-15 11:35:51 -0600

    basic navbar

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/final.html'))
+  res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 
 app.get('/whatif', ((req, res) => {
